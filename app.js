68969b3f13c73236855f1e7c4291f176ed74136f
Castanea02 <ahrhk2000@gmail.com>
2023-03-02 18:32:45 +0900

    classList

--- app.js
+++ app.js
@@ -1,44 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  h1.style.color = "blue";
-}
+  const clickedClass = "active";
 
-function Mouseenter() {
-  //마우스가  들어갈  때
-  h1.innerText = "Mouse is here!";
-}
-
-function Mouseleave() {
-  //마우스가  나갈 때
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  //창 크기 변경 할 때
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  //사용자가 복사할 때
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  //와이파이 연결 안 됨
-  alert("SOS no Wifi");
-}
-
-function handleWindowOnline() {
-  //와이파이 연결
-  alert("yes Wifi!");
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
+  } else {
+    h1.classList.add(clickedClass);
+  }
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", Mouseenter);
-h1.addEventListener("mouseleave", Mouseleave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
