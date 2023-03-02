e05ee3e7923ae75b9a5dba2510110790e2fdf5b6
Castanea02 <ahrhk2000@gmail.com>
2023-03-02 17:47:47 +0900

    Mouse Events

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const title = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  console.log("title was clicked!");
+  title.style.color = "blue";
 }
 
-title.addEventListener("click");
+function Mouseenter() {
+  title.innerText = "Mouse is here!";
+}
+
+function Mouseleave() {
+  title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", Mouseenter);
+title.addEventListener("mouseleave", Mouseleave);
