aa38c82d2eb95a55179e74b811893e48ce3fe2b5
Castanea02 <ahrhk2000@gmail.com>
2023-03-02 21:01:19 +0900

    Getting Username

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function handleTitleClick() {
-  h1.classList.toggle("active");
+const link = document.querySelector("a");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault(); //브라우저 기본동작 방지(submit 새로고침 방지)
+  loginForm.classList.add(HIDDEN_CLASSNAME); //로그인 폼 CSS 숨기기
+
+  const username = loginInput.value; //사용자 이름 저장
+  greeting.innerText = `Hello ${username}`; //입력 값 표현
+  greeting.classList.remove(HIDDEN_CLASSNAME); //사용자 입력값 포함 숨김 제거후 표현
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginForm.addEventListener("submit", onLoginSubmit);
