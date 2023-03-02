f56919337804dd460f5dac385849496e11abc914
Castanea02 <ahrhk2000@gmail.com>
2023-03-02 18:02:16 +0900

    window Event

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  title.style.color = "blue";
+  h1.style.color = "blue";
 }
 
 function Mouseenter() {
-  title.innerText = "Mouse is here!";
+  //마우스가  들어갈  때
+  h1.innerText = "Mouse is here!";
 }
 
 function Mouseleave() {
-  title.innerText = "Mouse is gone!";
+  //마우스가  나갈 때
+  h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", Mouseenter);
-title.addEventListener("mouseleave", Mouseleave);
+function handleWindowResize() {
+  //창 크기 변경 할 때
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  //사용자가 복사할 때
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  //와이파이 연결 안 됨
+  alert("SOS no Wifi");
+}
+
+function handleWindowOnline() {
+  //와이파이 연결
+  alert("yes Wifi!");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", Mouseenter);
+h1.addEventListener("mouseleave", Mouseleave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
